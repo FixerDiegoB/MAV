74e5fde5d0a2ad35b65bb98bd2172a63f65d8ada
JhoelAlmendrades <54906484+JhoelAlmendrades@users.noreply.github.com>
2022-10-11 16:27:28 -0500

    tablero

--- Assets/Scripts/Fichas.cs
+++ Assets/Scripts/Fichas.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ficha : MonoBehaviour
+public class Fichas : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
