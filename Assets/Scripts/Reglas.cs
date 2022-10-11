c61be583ff9df9f3a1d7bfe96d2f18843a0f55f5
Diego Bayes Santos <57854488+FixerDiegoB@users.noreply.github.com>
2022-10-11 17:44:00 -0500

    Scripts updated

---
+++ Assets/Scripts/Reglas.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reglas : MonoBehaviour
+{
+    public float altura;
+    public GameObject piezaBlanca, piezaNegra, piezaReferencia;
+
+    private bool turno; // true = blanco, false = negro
+    private Ray ray;
+    private Vector3 posicion;
+    private GameObject nuevaPieza;
+
+    private void Start()
+    {
+        turno = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                if (hitInfo.collider.gameObject != null)
+                {
+                    posicion = hitInfo.collider.gameObject.transform.position;
+                    posicion = new Vector3(posicion.x, posicion.y + altura, posicion.z);
+                    if (turno)
+                    {
+                        nuevaPieza = Instantiate(piezaBlanca, posicion, Quaternion.identity);
+                        nuevaPieza.transform.parent = piezaReferencia.transform;
+                        nuevaPieza.transform.position = posicion;
+                    }
+                    else
+                    {
+                        nuevaPieza = Instantiate(piezaNegra, posicion, Quaternion.identity);
+                        nuevaPieza.transform.parent = piezaReferencia.transform;
+                        nuevaPieza.transform.position = posicion;
+                    }
+                    turno = !turno;
+                }
+            }
+        }
+    }
+
+}
