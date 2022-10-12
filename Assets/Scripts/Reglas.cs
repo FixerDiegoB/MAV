885f5a256ec8a4ff888c54d637b91fc01208ecbb
Diego Bayes Santos <57854488+FixerDiegoB@users.noreply.github.com>
2022-10-12 15:16:32 -0500

    Modificaciones menores

--- Assets/Scripts/Reglas.cs
+++ Assets/Scripts/Reglas.cs
@@ -8,8 +8,6 @@ public class Reglas : MonoBehaviour
     public GameObject piezaBlanca, piezaNegra, piezaReferencia;
 
     private bool turno; // true = blanco, false = negro
-    private Ray ray;
-    private Vector3 posicion;
     private GameObject nuevaPieza;
 
     private void Start()
@@ -21,31 +19,35 @@ public class Reglas : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hitInfo))
-            {
-                if (hitInfo.collider.gameObject != null)
-                {
-                    Debug.Log(hitInfo.collider.gameObject.name);
-                    posicion = hitInfo.collider.gameObject.transform.parent.position;
-                    posicion = new Vector3(posicion.x, altura, posicion.z);
-                    if (turno)
-                    {
-                        nuevaPieza = Instantiate(piezaBlanca, posicion, Quaternion.identity);
-                        nuevaPieza.transform.parent = piezaReferencia.transform;
-                        nuevaPieza.transform.position = posicion;
-                    }
-                    else
-                    {
-                        nuevaPieza = Instantiate(piezaNegra, posicion, Quaternion.identity);
-                        nuevaPieza.transform.parent = piezaReferencia.transform;
-                        nuevaPieza.transform.position = posicion;
-                    }
-                    turno = !turno;
-                }
-            }
+            ColocarPieza();
         }
     }
 
+    private void ColocarPieza()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            if (hitInfo.collider.gameObject != null)
+            {
+                Debug.Log(hitInfo.collider.gameObject.name);
+                Vector3 posicion = hitInfo.collider.gameObject.transform.parent.position;
+                posicion = new Vector3(posicion.x, altura, posicion.z);
+                if (turno)
+                {
+                    nuevaPieza = Instantiate(piezaBlanca, posicion, Quaternion.identity);
+                    nuevaPieza.transform.parent = piezaReferencia.transform;
+                    nuevaPieza.transform.position = posicion;
+                }
+                else
+                {
+                    nuevaPieza = Instantiate(piezaNegra, posicion, Quaternion.identity);
+                    nuevaPieza.transform.parent = piezaReferencia.transform;
+                    nuevaPieza.transform.position = posicion;
+                }
+                turno = !turno;
+            }
+        }
+    }
 }
