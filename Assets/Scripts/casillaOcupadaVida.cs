f15ab080d3f2157872a258ffe95ccb39fb125a7b
Kritzan Y <kritzan.sullca.e@uni.pe>
2022-10-12 23:33:49 -0500

    duracion_sonido

---
+++ Assets/Scripts/casillaOcupadaVida.cs
@@ -0,0 +1,19 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class casillaOcupadaVida : MonoBehaviour
+{
+    public float duracion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,duracion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
