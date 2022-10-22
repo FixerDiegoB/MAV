8e63f00f057843992e4dd9e368c1a52c6ef237a3
Diego Bayes Santos <57854488+FixerDiegoB@users.noreply.github.com>
2022-10-22 13:05:30 -0500

    Mills added

--- Assets/Scripts/Producción/Token.cs
+++ Assets/Scripts/Producción/Token.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Color { WHITE, BLACK};
+
 
 public class Token : MonoBehaviour
 {
     private bool formaMolino;
     private Cell casilla;
-    private Color color;
+    private Status color;
 
 
 }
