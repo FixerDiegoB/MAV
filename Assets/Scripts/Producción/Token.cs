329eeb6f30d4be76eaa8917de146dfa3b6035de7
alexmzztt <88528696+alexmzztt@users.noreply.github.com>
2022-10-24 10:38:05 -0500

    UpdateToken y prefab

--- Assets/Scripts/Producción/Token.cs
+++ Assets/Scripts/Producción/Token.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Token : MonoBehaviour { 
 
-public class Token : MonoBehaviour
-{
-    private bool formaMolino;
-    [HideInInspector]
-    public Cell cell;
-    [HideInInspector]
-    public Status color;
-    public Rules rules;
+public Cell cell;
+[HideInInspector]
+public Status color;
+public Rulescopia rules;
+public UnityEngine.Color defaultColor;
+public UnityEngine.Color newColor;
+public Renderer render;
 
+    private void OnMouseOver()
+    {
+        if (rules == null) return;
+        if (rules.phase == GamePhase.MOVE && rules.turn == Status.WHITE)
+        {
+            render = GetComponent<Renderer>();
+            render.material.color = newColor;
+        }
+        if (rules.phase == GamePhase.MOVE && rules.turn == Status.BLACK)
+        {
+            render = GetComponent<Renderer>();
+            render.material.color = newColor;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (rules == null) return;
+        if (rules.phase == GamePhase.MOVE && rules.turn == Status.WHITE)
+        {
+            render = GetComponent<Renderer>();
+            render.material.color = defaultColor;
+        }
+        if (rules.phase == GamePhase.MOVE && rules.turn == Status.BLACK)
+        {
+            render = GetComponent<Renderer>();
+            render.material.color = defaultColor;
+        }
+    }
+    
     private void OnMouseDown()
     {
         Debug.Log("adios");
